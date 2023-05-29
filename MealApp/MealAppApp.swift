d44aa25187e9669419979d9b20860a889625533b
Kshitija Shaktawat <kshaktaw@buffalo.edu>
2023-05-29 14:10:49 -0400

    Initial Commit

---
+++ MealApp/MealAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MealAppApp.swift
+//  MealApp
+//
+//  Created by Kshitija Shaktawat on 5/29/23.
+//
+
+import SwiftUI
+
+@main
+struct MealAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
